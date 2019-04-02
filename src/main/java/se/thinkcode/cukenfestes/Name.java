7513096f94a3ae041422ef153ab7c405e7f89e84
Thomas Sundberg <tsu@kth.se>
2019-04-02 14:29:30 +0200

    Use domain types instead of sending around strings

--- src/main/java/se/thinkcode/cukenfestes/Name.java
+++ src/main/java/se/thinkcode/cukenfestes/Name.java
@@ -1,7 +1,33 @@
 package se.thinkcode.cukenfestes;
 
+import java.util.Objects;
+
 public class Name {
+    private String name;
+
     public Name(String name) {
-        throw new RuntimeException("Not yet implemented");
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
--- src/main/java/se/thinkcode/cukenfestes/Task.java
+++ src/main/java/se/thinkcode/cukenfestes/Task.java
@@ -1,7 +1,33 @@
 package se.thinkcode.cukenfestes;
 
+import java.util.Objects;
+
 public class Task {
+    private String name;
+
     public Task(String name) {
-        throw new RuntimeException("Not yet implemented");
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(name, task.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
