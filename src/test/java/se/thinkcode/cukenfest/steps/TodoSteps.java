61799258f22021110f5ba9aa7cd4edb4dda1ec12
Thomas Sundberg <tsu@kth.se>
2019-04-05 10:57:55 +0100

    Prepare a starting point for the example

--- src/test/java/se/thinkcode/cukenfest/steps/TodoSteps.java
+++ src/test/java/se/thinkcode/cukenfest/steps/TodoSteps.java
@@ -1,11 +1,11 @@
-package se.thinkcode.cukenfestes.steps;
+package se.thinkcode.cukenfest.steps;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import se.thinkcode.cukenfestes.todolist.Owner;
-import se.thinkcode.cukenfestes.todolist.Task;
-import se.thinkcode.cukenfestes.todolist.TodoList;
+import se.thinkcode.cukenfest.todolist.Owner;
+import se.thinkcode.cukenfest.todolist.Task;
+import se.thinkcode.cukenfest.todolist.TodoList;
 
 import java.util.List;
 
